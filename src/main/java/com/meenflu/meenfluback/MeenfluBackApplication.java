27b73323a095bd1971575a9f0731bd1ac9f3b332
SrMule <eduardo.garciao@usm.cl>
2024-04-14 23:13:02 -0400

    first commit

---
+++ src/main/java/com/meenflu/meenfluback/MeenfluBackApplication.java
@@ -0,0 +1,13 @@
+package com.meenflu.meenfluback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeenfluBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeenfluBackApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/meenflu/meenfluback/Models/Local.java
@@ -0,0 +1,5 @@
+package com.meenflu.meenfluback.Models;
+
+public class Local {
+    
+}
